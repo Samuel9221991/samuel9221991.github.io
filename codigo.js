366a9fc7bd5e71bd3fdf7dab9756affafa6a6394
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-07-07 18:05:58 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,15 @@
+let puntero = document.getElementById('rayita');
+let consola = document.getElementById('consola');
+let todo = document.getElementById('texto');
+
+var i = 1;
+
+setTimeout(() => {
+    var puntero_estilos = window.getComputedStyle(puntero);
+    var puntero_valor = puntero_estilos.getPropertyValue('width');
+    console.log(`${puntero_valor} i: ${i}`);
+    puntero.innerHTML = "<p>LENGUAJES:</p> <p>[1] Python</p> <p>[2] Arduino (C)</p> <p>[3] C#</p> <p>[4] JavaScript</p> <p>[5] PHP</p> <p>[6] CSS</p> <p>[7] HTML</p>";
+    puntero.className = "cambio_puntero";
+    consola.className += "agrandar_consola";
+    todo.innerHTML += " <span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span><span class='terminal_simbolo'>$</span><span id='rayita_oculta'>_</span>"
+} , 3000);
