ac9bb30d56d33d24abe9c4f1e8aca72ec9743625
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-07-07 18:19:12 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -8,7 +8,7 @@ setTimeout(() => {
     var puntero_estilos = window.getComputedStyle(puntero);
     var puntero_valor = puntero_estilos.getPropertyValue('width');
     console.log(`${puntero_valor} i: ${i}`);
-    puntero.innerHTML = "<p>LENGUAJES:</p> <p>[1] Python</p> <p>[2] Arduino (C)</p> <p>[3] C#</p> <p>[4] JavaScript</p> <p>[5] PHP</p> <p>[6] CSS</p> <p>[7] HTML</p>";
+    puntero.innerHTML = "<br>LENGUAJES:<br><br> [1] Python<br> [2] Arduino (C)<br> [3] C#<br> [4] JavaScript<br> [5] PHP<br> [6] CSS<br> [7] HTML<br>";
     puntero.className = "cambio_puntero";
     consola.className += "agrandar_consola";
     todo.innerHTML += " <span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span><span class='terminal_simbolo'>$</span> <span id='rayita_oculta'>_</span>"
