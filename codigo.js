05a7ff8eb410120caeba520a1453ed601ba37092
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-07-07 18:07:20 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -11,5 +11,5 @@ setTimeout(() => {
     puntero.innerHTML = "<p>LENGUAJES:</p> <p>[1] Python</p> <p>[2] Arduino (C)</p> <p>[3] C#</p> <p>[4] JavaScript</p> <p>[5] PHP</p> <p>[6] CSS</p> <p>[7] HTML</p>";
     puntero.className = "cambio_puntero";
     consola.className += "agrandar_consola";
-    todo.innerHTML += " <span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span><span class='terminal_simbolo'>$</span><span id='rayita_oculta'>_</span>"
+    todo.innerHTML += " <span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span><span class='terminal_simbolo'>$</span> <span id='rayita_oculta'>_</span>"
 } , 3000);
