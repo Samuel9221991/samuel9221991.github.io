51a1e618562f4e1701b32121e604782cf7e6ccb8
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-07-07 20:30:58 -0500

    Add files via upload

---
+++ pagina_contacto.js
@@ -0,0 +1,10 @@
+let consola_menu = document.getElementById('mini_consola');
+let todo_texto = document.getElementById('todo_consola_menu');
+let web = document.getElementById('principal');
+
+
+setTimeout(() => {
+    consola_menu.className += "agrandar_consola";
+    todo_texto.innerHTML = "<span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/contacto</span> <span class='terminal_simbolo'>$</span><span id='texto_consola'> <span id='rayita_menu'>_</span></span>"
+    web.className = "visible";
+} , 2300);
