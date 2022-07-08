482e9312ef49bc244e0158ce06bf518c7e33b79e
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-07-07 19:56:58 -0500

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -5,6 +5,6 @@ let web = document.getElementById('principal');
 
 setTimeout(() => {
     consola_menu.className += "agrandar_consola";
-    todo_texto.innerHTML = "<span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span> <span class='terminal_simbolo'>$</span><span id='texto_consola'><span id='rayita_menu'>_</span></span>"
+    todo_texto.innerHTML = "<span class='terminal_usuario'>user@web</span><span class='terminal_puntitos'>:</span><span class='terminal_ubicacion'>~/inicio</span> <span class='terminal_simbolo'>$</span><span id='texto_consola'> <span id='rayita_menu'>_</span></span>"
     web.className = "visible";
 } , 2300);
